21a7f9ae29b549bcc75befe7caa71921bf78f0a1
Andreas Olsson <andreasolsson2020@outlook.com>
2025-04-23 12:18:36 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import ProjectList from "./Components/ProjectList"
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ProjectDetails from "./Components/ProjectDetails";
 
-
-
 function App() {
- 
-
   return (
-    <Router>
-      <Routes>
-           
-        <Route path="/" element={<ProjectList projects={[]} />} />
-        <Route path="/project/:id" element={<ProjectDetails  />} />
-      </Routes>
-    </Router>
-  )
+    <Routes>
+      <Route path="/" element={<ProjectList projects={[]} />} />
+      <Route path="/project/:id" element={<ProjectDetails />} />
+    </Routes>
+  );
 }
 
 export default App
